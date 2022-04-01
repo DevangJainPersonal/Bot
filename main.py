5f3b5378bc753eee708233f5702ab7d4139a76ed
DevangJainPersonal <93637768+DevangJainPersonal@users.noreply.github.com>
2022-04-01 14:41:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,111 @@
+import os
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+Bot = Client(
+    "Remove Background Bot",
+    bot_token=os.environ.get("BOT_TOKEN"),
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH")
+)
+
+START_TEXT = """Hello {},
+I am a media background remover bot. Send me a photo I will send the photo without background.
+Made by 👨"""
+HELP_TEXT = """**More Help**
+- Just send me a photo
+- I will download it
+- I will send the photo without background
+Made by @FayasNoushad"""
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+            InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ],
+        [
+            InlineKeyboardButton('Help', callback_data='help'),
+            InlineKeyboardButton('About', callback_data='about'),
+            InlineKeyboardButton('Close', callback_data='close')
+        ]
+    ]
+)
+HELP_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Home', callback_data='home'),
+            InlineKeyboardButton('About', callback_data='about'),
+            InlineKeyboardButton('Close', callback_data='close')
+        ]
+    ]
+)
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Home', callback_data='home'),
+            InlineKeyboardButton('Help', callback_data='help'),
+            InlineKeyboardButton('Close', callback_data='close')
+        ]
+    ]
+)
+ERROR_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Help', callback_data='help'),
+            InlineKeyboardButton('Close', callback_data='close')
+        ]
+    ]
+)
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Join Updates Channel', url='https://telegram.me/FayasNoushad')
+        ]
+    ]
+)
+
+@Bot.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await start(bot, update, cb=True)
+    elif update.data == "help":
+        await help(bot, update, cb=True)
+    elif update.data == "about":
+        await about(bot, update, cb=True)
+    else:
+        await update.message.delete()
+        
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update, cb=False):
+    text=START_TEXT.format(update.from_user.mention)
+    if cb:
+        await update.message.edit_text(
+            text=text,
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.reply_text(
+            text=text,
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS,
+            quote=True
+        )
+
+
+@Bot.on_message(filters.private & filters.command(["help"]))
+async def help(bot, update, cb=False):
+    if cb:
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.reply_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True,
+            quote=True
+        )
